a3805060e49e57cb8f4bc51180456cc6a3ab003e
fgarnadi <fajar.garnadi@gmail.com>
2025-03-29 17:51:51 +0700

    add exercises

---
+++ src/widgets/mod.rs
@@ -0,0 +1,9 @@
+pub mod button;
+pub mod label;
+pub mod window;
+pub mod widget;
+
+pub use self::button::Button;
+pub use self::label::Label;
+pub use self::window::Window;
+pub use self::widget::Widget;
